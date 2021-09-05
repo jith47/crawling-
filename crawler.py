54270c4e598b1150a04a09bcabcb266fbf219b4b
Jithin Thomas <74818824+jith47@users.noreply.github.com>
2021-09-06 01:38:46 +0530

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -3,34 +3,34 @@ from scrapy.crawler import CrawlerProcess
 
 
 def parse_items(response):
-        breadcrumbs = response.xpath('//a[@class="breadcrumbs-list__link"]/text()').getall()
-        image_url = response.xpath('//a[@class="show-gallery"]/@href').get()
-        brand = response.xpath("//h1[@class='title']/text()").get()
-        pn = response.xpath("//span[@class='product-detail-label']/text()").get()
-        pn1 = response.xpath("//a[@href='/collections/carbon38']/text()").get()
-        product_name = pn+pn1
-        pr = response.xpath('//span[contains(.,"$")]/text()').get()
-        price = str(pr)
-        reviews = response.xpath("//div[@class='okeReviews-reviewsSummary-ratingCount']/span/text()").get()
-        if reviews is None:
-            reviews = "0 Reviews"
-        size = response.xpath('//*[@id="SingleOptionSelector-1"]/option/text()').getall()
-        colour = response.xpath('//*[@id="SingleOptionSelector-0"]/option/text()').getall()
-        t = response.xpath('/html/head/meta[4]/@content').get()
-        description = str(t)
-        sku = response.xpath('//p[contains(.,"SKU")]/text()').get()
-        yield {
-            'breadcrumbs': breadcrumbs,
-            'image_url': response.css('a.show-gallery').xpath('@href').get(),
-            'brand': response.css("h1.title::text").get(),
-            'product_name': pd_name,
-            'price': price.strip(),
-            'reviews': reviews,
-            'colour': colour,
-            'sizes': size,
-            'description': desc.strip(),
-            'sku': sku
-        }
+    breadcrumbs = response.xpath('//a[@class="breadcrumbs-list__link"]/text()').getall()
+    image_url = response.xpath('//a[@class="show-gallery"]/@href').get()
+    brand = response.xpath("//h1[@class='title']/text()").get()
+    pn = response.xpath("//span[@class='product-detail-label']/text()").get()
+    pn1 = response.xpath("//a[@href='/collections/carbon38']/text()").get()
+    product_name = pn+pn1
+    pr = response.xpath('//span[contains(.,"$")]/text()').get()
+    price = str(pr)
+    reviews = response.xpath("//div[@class='okeReviews-reviewsSummary-ratingCount']/span/text()").get()
+    if reviews is None:
+        reviews = "0 Reviews"
+    size = response.xpath('//*[@id="SingleOptionSelector-1"]/option/text()').getall()
+    colour = response.xpath('//*[@id="SingleOptionSelector-0"]/option/text()').getall()
+    t = response.xpath('/html/head/meta[4]/@content').get()
+    description = str(t)
+    sku = response.xpath('//p[contains(.,"SKU")]/text()').get()
+    yield {
+        'breadcrumbs': breadcrumbs,
+        'image_url': response.css('a.show-gallery').xpath('@href').get(),
+        'brand': response.css("h1.title::text").get(),
+        'product_name': pd_name,
+        'price': price.strip(),
+        'reviews': reviews,
+        'colour': colour,
+        'sizes': size,
+        'description': desc.strip(),
+        'sku': sku
+    }
 
 class ScrapSpider(scrapy.Spider):
     name = 'posts'
